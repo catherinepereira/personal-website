3e4274f8b638ac05d748d67cb14ba340930c451d
Cat Pereira <catherinelpa@gmail.com>
2025-04-22 16:13:36 -0700

    rm github pages config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-	/* config options here */
-	output: "export",
-	basePath: "/personal-website",
-	images: {
-		unoptimized: true,
-	},
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
