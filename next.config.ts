cf93b0a58c4d858924549b92768d62b811b5cb28
Cat Pereira <catherinelpa@gmail.com>
2025-04-22 16:03:35 -0700

    Fix path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: "export",
-	basePath: "/portfolio",
+	basePath: "/personal-website",
 	images: {
 		unoptimized: true,
 	},
